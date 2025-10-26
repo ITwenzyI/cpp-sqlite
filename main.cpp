6d6d32bd43f7eb93921124500f1dced75e8c569f
Kilian <kischoenhaar@gmail.com>
2025-10-26 19:37:31 +0100

    initialized program

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * File: main
+ * Author: Kilian | ItwenzyI
+ * Created: 26.10.2025
+ * Description: 
+ */
+//
+
+#include <iostream>
+
+int main (){
+
+    std::cout << "Hello World!\n";
+
+
+    return 0;
+
+}
